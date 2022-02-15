fd4d2148c20594ceba779209332dc12c7c807577
Vale <valesteve91@gmail.com>
2022-02-15 21:45:09 +0000

    increased lambda timeout

--- lib/utils/lambdaScaffholding.ts
+++ lib/utils/lambdaScaffholding.ts
@@ -1,13 +1,15 @@
 import { Construct } from 'constructs';
 import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs"
 import { join } from "path"
+import { Duration } from 'aws-cdk-lib';
 
 export class LambdaScaffoldingGen extends NodejsFunction {
     constructor(parent: Construct, id: string, handlerName: string, env = {}) {
         super(parent,id, {
             entry: join(__dirname, './handler.ts'),
             handler: handlerName,
-            environment: env
+            environment: env,
+            timeout: Duration.minutes(1)
         })
     }
 }
